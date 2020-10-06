f327cd5bd49ad3657017f2c30139006d75c9adcf
Alyona Klekovkina <like.the.sunshine.17@gmail.com>
2020-10-06 14:42:57 +0300

    Stage #2: Multiplication by number

--- processor.py
+++ processor.py
@@ -8,27 +8,22 @@ def get_matrix(row, column):
     return matrix
 
 
-def sum_matrices(a, b):
-    summed_matrix = []
+def multiply_matrix(x):
+    multiplied_matrix = []
     for i in range(int(n_1)):
-        summed_matrix.append([])
+        multiplied_matrix.append([])
         for j in range(int(m_1)):
-            summed_matrix[i].append(a[i][j] + b[i][j])
-    return summed_matrix
+            multiplied_matrix[i].append(matrix_a[i][j] * x)
+    return multiplied_matrix
 
 
 row_and_column_1 = input()
 n_1, m_1 = row_and_column_1.split()
 matrix_a = get_matrix(int(n_1), int(m_1))
-row_and_column_2 = input()
-n_2, m_2 = row_and_column_2.split()
-matrix_b = get_matrix(int(n_2), int(m_2))
+multiplier = int(input())
+result = multiply_matrix(multiplier)
 
-if row_and_column_1 != row_and_column_2:
-    print("ERROR")
-else:
-    matrix_c = sum_matrices(matrix_a, matrix_b)
-    for k in matrix_c:
-        print(' '.join(map(str, k)))
+for k in result:
+    print(' '.join(map(str, k)))
 
 
